c8dc99886a618ead1884a07cd69169ac40a6f7d0
alvarofuentes13 <muchoalvaro@gmail.com>
2025-02-25 12:45:21 +0100

    Security Config

---
+++ src/main/java/com/trabajoFinal/GameVerseBackEnd/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.trabajoFinal.GameVerseBackEnd.config;
+
+import com.trabajoFinal.GameVerseBackEnd.security.JwtAuthenticationFilter;
+import com.trabajoFinal.GameVerseBackEnd.security.JwtUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    public SecurityConfig(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/usuarios"
+                        )
+                        .permitAll() // Permitir acceso público
+                        .anyRequest().authenticated() // Proteger el resto de las rutas
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(this.jwtUtil),
+                        org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+}
